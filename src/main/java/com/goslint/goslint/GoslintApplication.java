4d539657643b1ced3928e3e00dd1861bf1044c73
Mario <mario.ochoa01@uceva.edu.co>
2025-10-01 20:53:04 -0500

    Creación del backend en Spring Boot para la aplicación móvil de Goslint Judge

---
+++ src/main/java/com/goslint/goslint/GoslintApplication.java
@@ -0,0 +1,13 @@
+package com.goslint.goslint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoslintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoslintApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goslint/goslint/GoslintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goslint.goslint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoslintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
